b511fa0d05137b6486f40f02e7f9d5a563df8439
Max <starosvetskiym@gmail.com>
2021-02-10 10:17:38 +0300

    Add new app.

--- survey_system/survey_system/settings.py
+++ survey_system/survey_system/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'surveys.apps.SurveysConfig',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
